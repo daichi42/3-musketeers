b1971c785476ef4b780c255bb315ddf492254f14
daichi42 <stefsan42@gmail.com>
2020-03-10 14:26:33 +0100

    most test

--- currency/index.test.js
+++ currency/index.test.js
@@ -38,39 +38,87 @@ beforeEach(() => {
       }
     });
 });
+const currency = require('./');
 
 test('convert 1 USD to EUR', async () => {
-  cli.module.exports.start();
+  var ammount = 1
+  var from = 'USD'
+  var to = 'EUR'
+  const opts = {ammount,from,to}
+  result = await currency(opts)
+  console.log("convert 1 USD to EUR")
+  console.log(`${ammount} ${from} = ${result} ${to}`)
 });
 
 test('convert 1 USD to USD', async () => {
-  throw new Error('test not yet defined... remove the throw and write your test here');
+  var ammount = 1
+  var from = 'USD'
+  var to = 'USD'
+  const opts = {ammount,from,to}
+  result = await currency(opts)
+  console.log("convert 1 USD to USD")
+  console.log(`${ammount} ${from} = ${result} ${to}`)
 });
 
 test('convert 1 EUR to USD', async () => {
-  throw new Error('test not yet defined... remove the throw and write your test here');
+  var ammount = 1
+  var from = 'EUR'
+  var to = 'USD'
+  const opts = {ammount,from,to}
+  result = await currency(opts)
+  console.log("convert 1 EUR to USD")
+  console.log(`${ammount} ${from} = ${result} ${to}`)
 });
 
 test('convert 1 BTC to USD', async () => {
-  throw new Error('test not yet defined... remove the throw and write your test here');
+  var ammount = 1
+  var from = 'BTC'
+  var to = 'USD'
+  const opts = {ammount,from,to}
+  result = await currency(opts)
+  console.log("convert 1 BTC to USD")
+  console.log(`${ammount} ${from} = ${result} ${to}`)
 });
 
 test('convert 1 BTC to EUR', async () => {
-  throw new Error('test not yet defined... remove the throw and write your test here');
+  var ammount = 1
+  var from = 'BTC'
+  var to = 'EUR'
+  const opts = {ammount,from,to}
+  result = await currency(opts)
+  console.log("convert 1 BTC to EUR")
+  console.log(`${ammount} ${from} = ${result} ${to}`)
 });
 
 test('convert without arguments', async () => {
-  throw new Error('test not yet defined... remove the throw and write your test here');
+  result = await currency({})
+  console.log("convert convert without arguments")
+  console.log(`${1} ${"USD"} = ${result} ${"BTC"}`)
+
 });
 
 test('convert with amount only', async () => {
-  throw new Error('test not yet defined... remove the throw and write your test here');
+  var ammount = 50
+  result = await currency({ammount})
+  console.log("convert convert with amount only")
+  console.log(`${ammount} ${"USD"} = ${result} ${"BTC"}`)
 });
 
 test('convert with amount and (from) currency only', async () => {
-  throw new Error('test not yet defined... remove the throw and write your test here');
+  var ammount = 50
+  var from = 'EUR'
+  const opts = {ammount,from}
+  result = await currency(opts)
+  console.log("convert convert with amount only and (from) currency only")
+  console.log(`${ammount} ${from} = ${result} ${"BTC"}`)
 });
 
 test('convert without a correct `from` or `to` currency value', async () => {
-  throw new Error('test not yet defined... remove the throw and write your test here');
+  var ammount = 1
+  var from = 'banane'
+  var to = 'pomme'
+  const opts = {ammount,from,to}
+  result = await currency(opts)
+  console.log("convert without a correct `from` or `to` currency value")
+  console.log(`${ammount} ${from} = ${result} ${to}`)
 });
